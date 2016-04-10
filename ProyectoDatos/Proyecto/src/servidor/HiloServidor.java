95745db6bf1f8ead3093bda8c9b486579e1b4df2
Oscar Leif <oscar-leif@outlook.com>
2016-04-09 23:09:13 -0500

    Cambio de prueba creado por Oscar.

--- ProyectoDatos/Proyecto/src/servidor/HiloServidor.java
+++ ProyectoDatos/Proyecto/src/servidor/HiloServidor.java
@@ -12,41 +12,40 @@ import java.util.List;
 
 import DAO.PasajeroDAO;
 
-public class HiloServidor implements Runnable {
+public class HiloServidor implements Runnable
+{
 
-	private Conexion con;
-	private DataInputStream in;
-	private DataOutputStream out;
+    private Conexion         con;
+    private DataInputStream  in;
+    private DataOutputStream out;
 
+    private Socket           socketPasajero;
+    // private PrintWriter out;
+    // private BufferedReader in;
 
-	private Socket socketPasajero;
-	// private PrintWriter out;
-	// private BufferedReader in;
+    private PasajeroDAO      pasajeroDAO;
 
-	private PasajeroDAO pasajeroDAO;
+    public HiloServidor(Conexion con, DataInputStream in, DataOutputStream out) throws Exception
+    {
+        // pasajeroDAO = new PasajeroDAO(this);
+        // socketPasajero = new Socket("localhost", Servidor.PUERTO);
+        // out = new PrintWriter(socketPasajero.getOutputStream(), true);
+        // in = new BufferedReader(new
+        // InputStreamReader(socketPasajero.getInputStream()));
 
-	public HiloServidor(Conexion con,DataInputStream in, DataOutputStream out) throws Exception {
-		// pasajeroDAO = new PasajeroDAO(this);
-		// socketPasajero = new Socket("localhost", Servidor.PUERTO);
-		// out = new PrintWriter(socketPasajero.getOutputStream(), true);
-		// in = new BufferedReader(new
-		// InputStreamReader(socketPasajero.getInputStream()));
+    }
 
+    private void procesarPeticion()
+    {
+        /*
+         * 1. lee un string sql 2. Clasifica la peticion sql (insert, update o delete) select 3. Si es (insert, update o delete) ejecuto la instrucion y retorno si o no 4. si es un select ejecuto la instrucion y retorno objeto con los datos de la consulta o un null;
+         */
 
-	}
+    }
 
-	private void procesarPeticion() {
-		/*
-		 * 1. lee un string sql
-		 * 2. Clasifica la peticion sql (insert, update o delete) select
-		 * 3. Si es (insert, update o delete) ejecuto la instrucion y retorno si o no
-		 * 4. si es un select ejecuto la instrucion y retorno objeto con los datos de la consulta o un null;
-		*/ 
-		
-	}
-
-	public void run() {
-		procesarPeticion();
-	}
+    public void run()
+    {
+        procesarPeticion();
+    }
 
 }
